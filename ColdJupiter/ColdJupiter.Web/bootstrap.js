96ba29089dcd1f4f6c66a0d78e8337cbb497404a
mattiaerre <mattia.richetto@gmail.com>
2015-06-19 09:33:46 +0100

    w/ bootstrap

---
+++ ColdJupiter/ColdJupiter.Web/bootstrap.js
@@ -0,0 +1,10 @@
+﻿// set PORT=1980
+// node bootstrap.js (this file)
+
+var app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+    console.log('express server listening on port ' + server.address().port);
+});
